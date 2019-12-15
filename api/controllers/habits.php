5667174678fce543ea9d2f098806226ce12720d1
punitha86 <punith.job@gmail.com>
2019-12-14 23:50:57 -0800

    made minor change to api for CORS issue

--- api/controllers/habits.php
+++ api/controllers/habits.php
@@ -4,11 +4,9 @@ include_once __DIR__ . '/../models/habits.php';
 header('Content-Type: application/json');
 
 if ($_REQUEST['action'] === 'INDEX') {
-  echo "IndexPage";
     echo json_encode(Habits::all());
 }
 elseif ($_REQUEST['action'] === 'POST') {
-  echo "Post page";
   $request_body = file_get_contents('php://input');
   $body_object = json_decode($request_body);
   $new_habit = new Habit(
@@ -24,7 +22,6 @@ elseif ($_REQUEST['action'] === 'POST') {
   echo json_encode($all_habits);
 }
 else if ($_REQUEST['action'] === 'UPDATE'){
-  echo "Update page";
   $request_body = file_get_contents('php://input');
   $body_object = json_decode($request_body);
   $updated_habit = new Habit($_REQUEST['id'],
